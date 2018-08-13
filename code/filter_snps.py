e2d3bb27a3596d64aaa34200fe9bea5cd3450b3c
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-13 14:40:18 -0400

    Added MAF option

--- code/filter_snps.py
+++ code/filter_snps.py
@@ -4,7 +4,7 @@ from collections import Counter
 import argparse
 import os.path
 
-def filter(line):
+def filter(line, maf_cutoff):
     line = line.rstrip()
     if line.startswith('#'):
         return(line)
@@ -14,13 +14,14 @@ def filter(line):
         geno_count = Counter(genos)
         if len(geno_count.most_common()) == 1:
             return
-        if (geno_count.most_common()[0][1]) < (len(genos)-len(genos)*0.05):
+        if (geno_count.most_common()[0][1]) < (len(genos)-len(genos)*maf_cutoff):
             return(line)
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-v','--vcf-input', required=True, help='')
     parser.add_argument('-o','--output-file',nargs='?', help='')
+    parser.add_argument('-m','--maf-cutoff',type=float, help='')
     parser.add_argument('-s','--stdout', action='store_true',help='')
     args = parser.parse_args()
 
@@ -31,9 +32,9 @@ def get_args():
     if not args.stdout and (not args.output_file):
         args.output_file = args.vcf_input + '.maf_filtered'
 
-    return(args.vcf_input, args.output_file)
+    return(args.vcf_input, args.output_file, args.maf_cutoff)
     
-def filt_all(vcf_input, output_file=None):
+def filt_all(vcf_input, output_file=None, maf_cutoff=0.05):
     if not output_file:
         of = sys.stdout
     else:
@@ -41,14 +42,14 @@ def filt_all(vcf_input, output_file=None):
 
     with open(vcf_input, 'r') as f:
         for line in f:
-            line = filter(line)
+            line = filter(line, maf_cutoff)
             if line:
                 print(line, file=of)
     
 
 def main():
-    vcf_input, output_file = get_args()
-    filt_all(vcf_input, output_file)
+    vcf_input, output_file, maf_cutoff = get_args()
+    filt_all(vcf_input, output_file, maf_cutoff)
 
 if __name__ == '__main__':
     main()
