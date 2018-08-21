8baef202d2553b462274fbb484b8a91dcdccbd10
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-21 09:37:58 -0400

    Added all arguments to function call

--- code/run_matrix_eqtl.py
+++ code/run_matrix_eqtl.py
@@ -16,6 +16,17 @@ def get_args():
     parser.add_argument('-o','--output-file',nargs='?', help='')
     parser.add_argument('-v','--p-value',type=float,nargs='?', help='')
     parser.add_argument('-q','--qq-plot',nargs='?', help='')
+    parser.add_argument('--trans-output-file', default="", help='')
+    parser.add_argument('--trans-p-value', default=0.0, help='')
+    parser.add_argument('--model', default='linear', choices={'linear','anova','linear_cross'},help='')
+    parser.add_argument('--cis-distance', default=1e6, help='')
+    parser.add_argument('--maf', default=0.0, help='')
+    parser.add_argument('--header',action='store_true', default=True, help='')
+    parser.add_argument('--row-names',action='store_true',default=True, help='')
+    parser.add_argument('--missing',default='NA',help='') 
+    parser.add_argument('--sep',default='\t',help='')
+
+    
     args = parser.parse_args()
 
     if not os.path.isfile(args.genotype_matrix):
@@ -31,18 +42,18 @@ def get_args():
     if not args.p_value:
         args.p_value = 0.05
 
-    return(args.genotype_matrix, args.genotype_positions, args.gene_expression_matrix, args.gene_positions, args.covariates, args.output_file, args.p_value, args.qq_plot)
+    return(args)
 
 def main():
 
-    genotype_matrix, genotype_positions, gene_expression_matrix, gene_positions, covariates, output_file, pval,qqplot = get_args()
+    args = get_args()
 
     r = ro.r
 
     r.source("code/mxeqtl.R")
     r.source("code/MatrixEQTL.R")
 
-    r.mxeqtl(genotype_matrix,genotype_positions,gene_expression_matrix,gene_positions,covariates=covariates,cis_output_file=output_file,cis_pval=pval,qq=qqplot)
+    r.mxeqtl(args.genotype_matrix,args.genotype_positions,args.gene_expression_matrix,args.gene_positions,covariates=args.covariates,cis_output_file=args.output_file,cis_pval=args.p_value, trans_output_file=args.trans_output_file, trans_pval=args.trans_p_value, cis_dist=args.cis_distance, MAF=args.maf, qq=args.qq_plot, model = args.model, header=args.header, rownames=args.row_names, missing=args.missing, sep=args.sep)
 
 if __name__ == '__main__':
     main()
