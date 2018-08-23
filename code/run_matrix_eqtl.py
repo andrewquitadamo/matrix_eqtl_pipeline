1e60fb35a3e9622ab5787c4b92d1d1104a9dca0d
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-23 06:52:42 -0400

    Updated argument defaults and types

--- code/run_matrix_eqtl.py
+++ code/run_matrix_eqtl.py
@@ -14,15 +14,15 @@ def get_args():
     parser.add_argument('-g','--gene-positions', required=True, help='')
     parser.add_argument('-c','--covariates', help='')
     parser.add_argument('-o','--output-file',nargs='?', help='')
-    parser.add_argument('-v','--p-value',type=float,nargs='?', help='')
+    parser.add_argument('-v','--p-value',type=float,default=0.05, help='')
     parser.add_argument('-q','--qq-plot',nargs='?', help='')
     parser.add_argument('--trans-output-file', default="", help='')
-    parser.add_argument('--trans-p-value', default=0.0, help='')
+    parser.add_argument('--trans-p-value', type=float,default=0.0, help='')
     parser.add_argument('--model', default='linear', choices={'linear','anova','linear_cross'},help='')
-    parser.add_argument('--cis-distance', default=1e6, help='')
+    parser.add_argument('--cis-distance', type=float, default=1e6, help='')
     parser.add_argument('--maf', default=0.0, help='')
-    parser.add_argument('--header',action='store_true', default=True, help='')
-    parser.add_argument('--row-names',action='store_true',default=True, help='')
+    parser.add_argument('--no-header',action='store_false', help='')
+    parser.add_argument('--no-rownames',action='store_false', help='')
     parser.add_argument('--missing',default='NA',help='') 
     parser.add_argument('--sep',default='\t',help='')
 
@@ -39,9 +39,6 @@ def get_args():
     if not args.qq_plot:
         args.qq_plot = 'MatrixEqtlQQPlot.pdf'
 
-    if not args.p_value:
-        args.p_value = 0.05
-
     return(args)
 
 def main():
@@ -53,7 +50,7 @@ def main():
     r.source("code/mxeqtl.R")
     r.source("code/MatrixEQTL.R")
 
-    r.mxeqtl(args.genotype_matrix,args.genotype_positions,args.gene_expression_matrix,args.gene_positions,covariates=args.covariates,cis_output_file=args.output_file,cis_pval=args.p_value, trans_output_file=args.trans_output_file, trans_pval=args.trans_p_value, cis_dist=args.cis_distance, MAF=args.maf, qq=args.qq_plot, model = args.model, header=args.header, rownames=args.row_names, missing=args.missing, sep=args.sep)
+    r.mxeqtl(args.genotype_matrix,args.genotype_positions,args.gene_expression_matrix,args.gene_positions,covariates=args.covariates,cis_output_file=args.output_file,cis_pval=args.p_value, trans_output_file=args.trans_output_file, trans_pval=args.trans_p_value, cis_dist=args.cis_distance, MAF=args.maf, qq=args.qq_plot, model = args.model, header=args.no_header, rownames=args.no_rownames, missing=args.missing, sep=args.sep)
 
 if __name__ == '__main__':
     main()
