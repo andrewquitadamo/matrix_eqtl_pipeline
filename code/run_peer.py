6c5fe7f1099c9cfa0a1b38bdac493561fc69743a
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-29 17:16:12 -0400

    PEP8 compliance

--- code/run_peer.py
+++ code/run_peer.py
@@ -7,18 +7,21 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects.vectors import DataFrame
 from check_file import check_file
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input-file', required=True, help='')
-    parser.add_argument('-n','--number-factors', required=True, help='')
-    parser.add_argument('-o','--output-file',nargs='?', help='')
+    parser.add_argument('-i', '--input-file', required=True, help='')
+    parser.add_argument('-n', '--number-factors', required=True, help='')
+    parser.add_argument('-o', '--output-file', help='')
     args = parser.parse_args()
 
     check_file(args.input_file)
 
-    args.output_file = args.output_file or (args.input_file + '.peer_factors_' + args.number_factors)
+    args.output_file = args.output_file or (
+        args.input_file + '.peer_factors_' + args.number_factors)
 
     return(args.input_file, args.number_factors, args.output_file)
+
 
 def main():
     r = ro.r
@@ -31,7 +34,8 @@ def main():
 
     factors = r['peer_function'](input_filename, num_factors)
 
-    write_table(factors, output_filename, col_names=True, row_names=False, sep="\t", quote=False)
+    write_table(factors, output_filename, col_names=True,
+                row_names=False, sep="\t", quote=False)
 
 if __name__ == '__main__':
     main()
