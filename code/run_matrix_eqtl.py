58db2e322133d7ff9f09662c9865b7e3ff55f7aa
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-29 18:54:04 -0400

    PEP8 compliance

--- code/run_matrix_eqtl.py
+++ code/run_matrix_eqtl.py
@@ -7,25 +7,30 @@ import argparse
 import os
 from check_file import check_file
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m','--genotype-matrix', required=True, help='')
-    parser.add_argument('-p','--genotype-positions', required=True, help='')
-    parser.add_argument('-e','--gene-expression-matrix', required=True, help='')
-    parser.add_argument('-g','--gene-positions', required=True, help='')
-    parser.add_argument('-c','--covariates', help='')
-    parser.add_argument('-o','--output-file',default='MatrixEqtlOutput', help='')
-    parser.add_argument('-v','--p-value',type=float,default=0.05, help='')
-    parser.add_argument('-q','--qq-plot',default='MatrixEqtlQQPlot.pdf', help='')
+    parser.add_argument('-m', '--genotype-matrix', required=True, help='')
+    parser.add_argument('-p', '--genotype-positions', required=True, help='')
+    parser.add_argument('-e', '--gene-expression-matrix',
+                        required=True, help='')
+    parser.add_argument('-g', '--gene-positions', required=True, help='')
+    parser.add_argument('-c', '--covariates', help='')
+    parser.add_argument('-o', '--output-file',
+                        default='MatrixEqtlOutput', help='')
+    parser.add_argument('-v', '--p-value', type=float, default=0.05, help='')
+    parser.add_argument('-q', '--qq-plot',
+                        default='MatrixEqtlQQPlot.pdf', help='')
     parser.add_argument('--trans-output-file', default="", help='')
-    parser.add_argument('--trans-p-value', type=float,default=0.0, help='')
-    parser.add_argument('--model', default='linear', choices={'linear','anova','linear_cross'},help='')
+    parser.add_argument('--trans-p-value', type=float, default=0.0, help='')
+    parser.add_argument('--model', default='linear',
+                        choices={'linear', 'anova', 'linear_cross'}, help='')
     parser.add_argument('--cis-distance', type=float, default=1e6, help='')
     parser.add_argument('--maf', default=0.0, help='')
-    parser.add_argument('--no-header',action='store_false', help='')
-    parser.add_argument('--no-rownames',action='store_false', help='')
-    parser.add_argument('--missing',default='NA',help='') 
-    parser.add_argument('--sep',default='\t',help='')
+    parser.add_argument('--no-header', action='store_false', help='')
+    parser.add_argument('--no-rownames', action='store_false', help='')
+    parser.add_argument('--missing', default='NA', help='')
+    parser.add_argument('--sep', default='\t', help='')
 
     args = parser.parse_args()
 
@@ -33,8 +38,8 @@ def get_args():
 
     return(args)
 
-def main():
 
+def main():
     args = get_args()
 
     r = ro.r
@@ -42,7 +47,14 @@ def main():
     r.source("code/mxeqtl.R")
     r.source("code/MatrixEQTL.R")
 
-    r.mxeqtl(args.genotype_matrix,args.genotype_positions,args.gene_expression_matrix,args.gene_positions,covariates=args.covariates,cis_output_file=args.output_file,cis_pval=args.p_value, trans_output_file=args.trans_output_file, trans_pval=args.trans_p_value, cis_dist=args.cis_distance, MAF=args.maf, qq=args.qq_plot, model = args.model, header=args.no_header, rownames=args.no_rownames, missing=args.missing, sep=args.sep)
+    r.mxeqtl(args.genotype_matrix, args.genotype_positions,
+             args.gene_expression_matrix, args.gene_positions,
+             covariates=args.covariates, cis_output_file=args.output_file,
+             cis_pval=args.p_value, trans_output_file=args.trans_output_file,
+             trans_pval=args.trans_p_value, cis_dist=args.cis_distance,
+             MAF=args.maf, qq=args.qq_plot, model=args.model,
+             header=args.no_header, rownames=args.no_rownames,
+             missing=args.missing, sep=args.sep)
 
 if __name__ == '__main__':
     main()
