da29b98474e4827ac88484cfa694f7d5bc84d077
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-29 18:58:27 -0400

    PEP8 compliance

--- code/remove_vcf_header.py
+++ code/remove_vcf_header.py
@@ -4,11 +4,12 @@ import argparse
 import os.path
 from check_file import check_file
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v','--vcf-file',required=True, help='')
-    parser.add_argument('-o','--output-file',nargs='?', help='')
-    parser.add_argument('-s','--stdout', action='store_true', help='')
+    parser.add_argument('-v', '--vcf-file', required=True, help='')
+    parser.add_argument('-o', '--output-file', nargs='?', help='')
+    parser.add_argument('-s', '--stdout', action='store_true', help='')
     args = parser.parse_args()
 
     check_file(args.vcf_file)
@@ -18,16 +19,18 @@ def get_args():
 
     return(args.vcf_file, args.output_file)
 
+
 def remove_header(input_file, output_file=None):
     if not output_file:
         of = sys.stdout
     else:
         of = open(output_file, 'w')
 
-    with open(input_file,'r') as f:
+    with open(input_file, 'r') as f:
         for line in f:
             if not line.startswith('##'):
-                print(line,file=of,end='')
+                print(line, file=of, end='')
+
 
 def main():
     input_file, output_file = get_args()
