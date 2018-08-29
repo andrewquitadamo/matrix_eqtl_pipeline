fc0d8a61dbfe098b16eda112dd504dbf584f092c
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-29 19:16:45 -0400

    PEP8 compliance

--- code/pc_covariates.py
+++ code/pc_covariates.py
@@ -6,12 +6,13 @@ from sklearn.decomposition import IncrementalPCA
 import pandas as pd
 from check_file import check_file
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v','--vcf-file', required=True, help='')
-    parser.add_argument('-o','--output-file',nargs='?', help='')
-    parser.add_argument('-n','--number-pcs',default=1,type=int,help='')
-    parser.add_argument('-s','--stdout',action='store_true', help='')
+    parser.add_argument('-v', '--vcf-file', required=True, help='')
+    parser.add_argument('-o', '--output-file', help='')
+    parser.add_argument('-n', '--number-pcs', default=1, type=int, help='')
+    parser.add_argument('-s', '--stdout', action='store_true', help='')
     args = parser.parse_args()
 
     check_file(args.vcf_file)
@@ -20,6 +21,7 @@ def get_args():
         args.output_file = args.vcf_file + '.pcs'
 
     return(args.vcf_file, args.output_file, args.number_pcs)
+
 
 def pca(genotypes, vcf_file, output_file, number_pcs):
     if not output_file:
@@ -33,8 +35,9 @@ def pca(genotypes, vcf_file, output_file, number_pcs):
 
     pcs = ""
 
-    for i in range(0,number_pcs):
-        pcs = pcs + "PC" + str(i+1) + "\t" + "\t".join(str(x) for x in ipca.components_[i]) + "\n"
+    for i in range(0, number_pcs):
+        pcs = pcs + "PC" + str(i+1) + "\t" + "\t".join(
+            str(x) for x in ipca.components_[i]) + "\n"
 
     with open(vcf_file, 'r') as f:
         header = f.readline().rstrip()
@@ -42,9 +45,10 @@ def pca(genotypes, vcf_file, output_file, number_pcs):
     print(header, file=of)
     print(pcs, end='', file=of)
 
+
 def main():
     vcf_file, output_file, number_pcs = get_args()
-    genotypes = pd.read_csv(vcf_file, sep = '\t', chunksize = 100000, index_col=0)
+    genotypes = pd.read_csv(vcf_file, sep='\t', chunksize=100000, index_col=0)
     pca(genotypes, vcf_file, output_file, number_pcs)
 
 if __name__ == '__main__':
