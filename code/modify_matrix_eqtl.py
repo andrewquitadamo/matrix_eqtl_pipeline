5cf8af874e6320264b35df26d4f42186de5f5f76
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-29 21:24:25 -0400

    PEP8 compliance

--- code/modify_matrix_eqtl.py
+++ code/modify_matrix_eqtl.py
@@ -3,16 +3,18 @@ import subprocess
 import sys
 import argparse
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o','--output-file',nargs='?', help='')
-    parser.add_argument('-s','--stdout', action='store_true', help='')
+    parser.add_argument('-o', '--output-file', help='')
+    parser.add_argument('-s', '--stdout', action='store_true', help='')
     args = parser.parse_args()
 
     if not args.stdout and not args.output_file:
         args.output_file = 'code/MatrixEQTL.R'
 
     return(args.output_file)
+
 
 def main():
     output_file = get_args()
@@ -21,21 +23,24 @@ def main():
     else:
         of = open(output_file, 'w')
 
-    subprocess.run(["wget", "https://cran.r-project.org/src/contrib/MatrixEQTL_2.2.tar.gz"], stdout=subprocess.DEVNULL)
-    subprocess.run(["tar", "-xvzf", "MatrixEQTL_2.2.tar.gz"], stdout=subprocess.DEVNULL)
+    subprocess.run(["wget",
+                   "https://cran.r-project.org/src/contrib/MatrixEQTL_2.2.tar.gz"],
+                   stdout=subprocess.DEVNULL)
+    subprocess.run(["tar", "-xvzf", "MatrixEQTL_2.2.tar.gz"],
+                   stdout=subprocess.DEVNULL)
 
     with open('MatrixEQTL/R/Matrix_eQTL_engine.R') as f:
         for line in f:
             if line.strip().startswith('message'):
                 if 'matched' in line:
-                    line = line.replace('message','mess = paste')
+                    line = line.replace('message', 'mess = paste')
                 else:
-                    line = line.replace('message','mess = paste0')
+                    line = line.replace('message', 'mess = paste0')
                 if line.strip().endswith(");"):
                     if 'rp' in line:
-                        line = line.replace(");",",'\\n',sep='');")
+                        line = line.replace(");", ", '\\n', sep='');")
                     else:
-                        line = line.replace(");",",'\\n');")
+                        line = line.replace(");", ", '\\n');")
                 if 'significant' in line:
                     print(line, file=of)
                     print(next(f), file=of)
