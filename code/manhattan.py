8324a4890530bdf45b36324fc1c5e4544f38609d
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2018-08-29 20:58:26 -0400

    PEP8 compliance

--- code/manhattan.py
+++ code/manhattan.py
@@ -8,16 +8,18 @@ from rpy2.robjects.vectors import DataFrame
 import numpy
 from check_file import check_file
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e','--eqtl-output-file', required=True, help='')
-    parser.add_argument('-l','--position-file',nargs='?', help='')
-    parser.add_argument('-p','--pdf-file',help='')
+    parser.add_argument('-e', '--eqtl-output-file', required=True, help='')
+    parser.add_argument('-l', '--position-file', help='')
+    parser.add_argument('-p', '--pdf-file', help='')
     args = parser.parse_args()
 
     check_file(args.eqtl_output_file)
 
     return(args.eqtl_output_file, args.position_file, args.pdf_file)
+
 
 def manhattan(eqtl_output_file, position_file, pdf_file):
     r = ro.r
@@ -27,6 +29,7 @@ def manhattan(eqtl_output_file, position_file, pdf_file):
     r.source("code/manhattan.R")
 
     r['manhattan'](eqtl_output_file, position_file, pdf_file)
+
 
 def main():
     eqtl_output_file, position_file, pdf_file = get_args()
